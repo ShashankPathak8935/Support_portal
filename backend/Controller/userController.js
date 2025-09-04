5eea8f0e9bcedb333c9672fa3a29abd69bc21373
ShashankPathak8935 <pathakshashank285@gmail.com>
2025-09-05 01:14:30 +0530

    login successfully

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -1,3 +1,4 @@
+const { raw } = require("body-parser");
 const User = require("../Models/userModel");
 // const catchAsync = require("../utils/catchAsync");
 
@@ -26,3 +27,25 @@ exports.SignUp = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.LogIn = async (req, res, next) => {
+  console.log("Login request body:", req.body);
+  try {
+    const { username, password } = req.body; // Adjusted to match frontend keys
+    if(!username || !password) {
+      return res.status(400).json({ message: "Username and password are required." });
+    }
+    const user = await User.findOne({
+      where: { email: username },
+      raw: true,
+    }
+    ); // Assuming username is email
+    if (!user || user.password !== password) {
+      return res.status(401).json({ message: "Invalid username or password." });
+    }
+    res.status(200).json({ message: "Login successful", user });
+  } catch (error) {
+    next(error);
+  }
+};
+
--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../Models/userModel");
-const userController = require("../Controller/userController")
+const userController = require("../Controller/userController");
 
 // Define your user-related routes here
 router.post("/signup", userController.SignUp);
-// router.post("/login", userController.LogIn);
+router.post("/login", userController.LogIn);
 // router.get("/users", userController.getAllUsers);
 // router.get("/users/:id", userController.getUserById);
 // router.put("/users/:id", userController.updateUser);
