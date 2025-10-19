e0861cbddb76688ac1b1940ea7a91994969b636c
ShashankPathak8935 <pathakshashank285@gmail.com>
2025-10-20 00:59:46 +0530

    localhost and neworking both check

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,13 @@
-const SERVER_URL = import.meta.env.VITE_SERVER_DEV_URL;
+const LOCAL_SERVER_URL = import.meta.env.VITE_LOCAL_SERVER_DEV_URL;
+const NETWORK_SERVER_URL = import.meta.env.VITE_NETWORK_SERVER_DEV_URL;
+
+// manually--> change the server url based on your network or local
+// export const SERVER_URL = LOCAL_SERVER_URL; // for local server
+// export const SERVER_URL = NETWORK_SERVER_URL; // for network server
+
+const isLocalHost = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+const SERVER_URL = isLocalHost ? LOCAL_SERVER_URL : NETWORK_SERVER_URL;
+
 
 // user api
 export const USER_API = `${SERVER_URL}/api/v1/users`;
